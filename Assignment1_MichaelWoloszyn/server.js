e399f03802f5424296e52cd6dbcc09c51fd8c594
991571925 <woloszym@sheridancollege.ca>
2022-02-09 21:49:28 -0500

    Revert "Added functions to java script"
    This reverts commit 8f18a28a3a9824f489d7ae5bab9165cb2c61dc8c.

--- Assignment1_MichaelWoloszyn/server.js
+++ Assignment1_MichaelWoloszyn/server.js
@@ -1,33 +1,8 @@
-function setForm() {
-    localStorage.setItem('Name', $('#n'));
-    localStorage.setItem('Email', $('#e'));
-    localStorage.setItem('Room', $('#r'));
-    localStorage.setItem('Width', $('#w'));
-    localStorage.setItem('Length', $('#l'));
-    localStorage.setItem('Colour', $('#c'));
-    localStorage.setItem('Paint', $('#p'));
-    fillTable();
-}
+'use strict';
+var http = require('http');
+var port = process.env.PORT || 1337;
 
-function fillTable() {
-    var table = $('<table>');
-    for (i = 0; i < 7; i++) {
-        if (i == 0)
-            localStorage.setItem('Name', $('#n'));
-        if (i == 1)
-            localStorage.setItem('Email', $('#e'));
-        if (i == 2)
-            localStorage.setItem('Room', $('#r'));
-        if (i == 3)
-            localStorage.setItem('Width', $('#w'));
-        if (i == 4)
-            localStorage.setItem('Length', $('#l'));
-        if (i == 5)
-            localStorage.setItem('Colour', $('#c'));
-        if (i == 6)
-            localStorage.setItem('Paint', $('#p'));
-        var row = $('<tr>').text(JSON.stringify(result));
-        table.append(row);
-    }
-    $('vtable').append(table);
-}
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
